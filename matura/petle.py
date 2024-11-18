e420e061676e3e2241db124a4d2527b86e7ba33d
Przemek Joniec <joniecprzemek098@gmail.com>
2024-11-18 20:27:15 +0100

    essa

--- matura/petle.py
+++ matura/petle.py
@@ -10,8 +10,8 @@
 # OOOOO
 
 
-# width = int(input("Podaj szerokosc trójkata: "))
-# height = int(input("Podaj wysokosc trójkata: "))
+# width = int(input("Podaj szerokosc: "))
+# height = int(input("Podaj wysokosc: "))
 
 # for i in range(height):
 #     if i == 0 or i == height - 1:
@@ -41,3 +41,55 @@
 #    AAAAAAAAA
 #   A         A
 #  A           A
+
+
+# width = int(input("Podaj szerokosc: "))
+# height = int(input("Podaj wysokosc: "))
+# thickness = int(input("Podaj grubosc: "))
+
+# for i in range (height+1):
+#     if i < int(height-thickness):
+#         print("L"*thickness)
+#     if i > int(height-thickness):
+#         print("L"*width)
+
+# LLL
+# LLL
+# LLL
+# LLL
+# LLL
+# LLL
+# LLLLLLLL
+# LLLLLLLL
+
+
+# height = int(input("Podaj wysokosc: "))
+
+# for i in range(height):
+#     if i == 0:
+#         print(" "*height+"X")
+#     if i > 0:
+#         print(" "*(height-i)+"X"*((i*2)+1))
+#      X
+#     XXX
+#    XXXXX
+#   XXXXXXX
+#  XXXXXXXXX
+# 
+
+
+# height = int(input("Podaj wysokosc: "))
+
+# for i in range(height-1):
+#     if i == 0:
+#         print(" "*(height+1)+"X")
+#     if i < height-2:
+#         print(" "*(height-i)+"X"+" "*((i*2)+1)+"X")
+#     else:
+#         print(" "*(height-i)+"X" * (2 * height - 1))      
+
+#       X
+#      X X
+#     X   X
+#    X     X
+#   XXXXXXXXX
