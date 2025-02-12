c7629bbbd4114a9713934de9c192cb1cb08bdc5b
Przemek Joniec <joniecprzemek098@gmail.com>
2025-02-12 22:41:54 +0100

    Kurs

--- Zadania/gry.py
+++ Zadania/gry.py
@@ -1,14 +1,14 @@
 gry = [
-    {'name': 'The Witcher 3: Wild Hunt', 'genre': 'RPG', 'rating': 9.8},
-    {'name': 'Minecraft', 'genre': 'Sandbox', 'rating': 9.5},
-    {'name': 'The Legend of Zelda: Breath of the Wild', 'genre': 'Action-Adventure', 'rating': 9.7},
-    {'name': 'Cyberpunk 2077', 'genre': 'RPG', 'rating': 8.6},
-    {'name': 'Dark Souls III', 'genre': 'Action RPG', 'rating': 9.4},
-    {'name': 'Red Dead Redemption 2', 'genre': 'Action-Adventure', 'rating': 9.9},
-    {'name': 'Fortnite', 'genre': 'Battle Royale', 'rating': 8.0},
-    {'name': 'Among Us', 'genre': 'Social Deduction', 'rating': 8.5},
-    {'name': 'Grand Theft Auto V', 'genre': 'Action-Adventure', 'rating': 9.6},
-    {'name': 'Overwatch', 'genre': 'First-Person Shooter', 'rating': 9.0}
+    {'name': 'The Witcher 3: Wild Hunt', 'genre': 'RPG', 'rating': 9.8, 'year': 2000},
+    {'name': 'Minecraft', 'genre': 'Sandbox', 'rating': 9.5, 'year': 2000},
+    {'name': 'The Legend of Zelda: Breath of the Wild', 'genre': 'Action-Adventure', 'rating': 9.7, 'year': 2000},
+    {'name': 'Cyberpunk 2077', 'genre': 'RPG', 'rating': 8.6, 'year': 2000},
+    {'name': 'Dark Souls III', 'genre': 'Action RPG', 'rating': 9.4, 'year': 2000},
+    {'name': 'Red Dead Redemption 2', 'genre': 'Action-Adventure', 'rating': 9.9, 'year': 2000},
+    {'name': 'Fortnite', 'genre': 'Battle Royale', 'rating': 8.0, 'year': 2000},
+    {'name': 'Among Us', 'genre': 'Social Deduction', 'rating': 8.5, 'year': 2000},
+    {'name': 'Grand Theft Auto V', 'genre': 'Action-Adventure', 'rating': 9.6, 'year': 2000},
+    {'name': 'Overwatch', 'genre': 'First-Person Shooter', 'rating': 9.0, 'year': 2000}
 ]
 
 while True:
@@ -38,15 +38,32 @@ while True:
             print("Gatunek: " + element['genre'])
             print("Ocena: " + str(element['rating']))
     elif opcja == "3":
-        print("test")
+        gry.sort(key=lambda x: x['rating'], reverse=True)
+        for element in gry[:5]:
+            print(element['name'])
     elif opcja == "4":
-        print("test")
+        znaleziono = False
+        gatunek = input("Podaj gatunek: ")
+        for element in gry:
+            if gatunek.upper() == element['genre']:
+                print(element['name'])
+                znaleziono = True
+            if not znaleziono:
+                print("Nie znaleziono")
+                break
     elif opcja == "5":
-        print("test")
+        nazwa = input("Podaj nazwe: ")
+        gatunek = input("Podaj gatunek: ")
+        ocena = input("Podaj ocene: ")
+        rok = input("Podaj rok wydania: ")
+        gry.append({'name': nazwa, 'genre': gatunek, 'rating': ocena, 'year': rok})
     elif opcja == "6":
         print("test")
     elif opcja == "7":
-        print("test")
+        nazwa = input("Podaj nazwe: ")
+        for element in gry:
+            if nazwa == element['name']:
+                gry.remove(element)
     elif opcja == "8":
         print("test")
     elif opcja == "9":
